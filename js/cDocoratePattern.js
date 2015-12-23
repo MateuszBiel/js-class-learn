63fe64258cd029a46bac8ed58a0466422c79a938
niepelny <Mateusz_Biel@epam.com>
2015-12-23 14:01:59 +0100

    checkout

---
+++ js/cDocoratePattern.js
@@ -0,0 +1,11 @@
+function decorate (classs, heightSmall, widthSmall) {
+    this.classs = classs;
+    this.heightSmall =heightSmall;
+    this.widthSmall = widthSmall;
+    this.calculateForce = function() {
+        console.log(this.classs.getDiameter()*this.classs.getDiameter());
+        var equasion = (1/12*(this.classs.getDiameter()*Math.pow(this.classs.getHeight(),3)-widthSmall*Math.pow(heightSmall,3)));
+        var def = (this.classs.getForce() * Math.pow( this.classs.getLenght(), 3)) / this.classs.getYoung() *equasion;
+        return def;
+    };
+}
